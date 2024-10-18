452c2bb1054df0e7af743bbd88b7248fa9f41305
Ethan <2629263780@qq.com>
2024-10-18 16:17:54 +0800

    Add files via upload
    Custom variable name.

---
+++ Ariable_name.h
@@ -0,0 +1,16 @@
+//
+// Created by ethan on 24-10-18.
+// Exact-width integer types
+//
+#pragma once
+#include <stdio.h>
+#include <stdint.h>
+
+typedef signed char  i8;
+typedef unsigned char   u8;
+typedef short  i16;
+typedef unsigned short  u16;
+typedef int  i32;
+typedef unsigned   u32;
+__MINGW_EXTENSION typedef long long  i64;
+__MINGW_EXTENSION typedef unsigned long long   u64;
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "Ariable_name.h"
+int main(void) {
+
+    u32 num;
+    printf("Please enter a number: \n");
+    scanf_s("%d", &num);
+
+    printf("You type number is : %d",num);
+    return 0;
+}
